f229f4f74f7c3d95e91a72713cc0a5239a4d4405
IHprogram <ihm20202020@gmail.com>
2021-06-23 12:23:50 +0900

    新規登録画面でテキストが正常に表示されているかを確認

--- src/__test__/user.test.js
+++ src/__test__/user.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import Login from '../components/Login'
+import Register from '../components/Register'
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
@@ -10,7 +11,6 @@ const store = createStore(reducer, applyMiddleware(thunk));
 
 describe('テキストが表示されているかの確認', () => {
   describe('ログイン画面のテキスト確認', () => {
-    // render(<Provider store={store}><MemoryRouter><Login /></MemoryRouter></Provider>);
     test('ログイン画面のタイトル確認', () => {
       render(<Provider store={store}><MemoryRouter><Login /></MemoryRouter></Provider>);
       expect(screen.getByText('ログイン')).toBeInTheDocument();
@@ -33,6 +33,33 @@ describe('テキストが表示されているかの確認', () => {
 
     test('パスワードのラベル確認', () => {
       render(<Provider store={store}><MemoryRouter><Login /></MemoryRouter></Provider>);
+      expect(screen.getByText('パスワード')).toBeInTheDocument();
+    })
+  })
+
+  describe('新規登録画面のテキスト確認', () => {
+    test('新規登録画面のタイトル確認', () => {
+      render(<Provider store={store}><MemoryRouter><Register /></MemoryRouter></Provider>);
+      expect(screen.getByText('新規登録')).toBeInTheDocument();
+    })
+
+    test('新規登録ボタンのテキスト確認', () => {
+      render(<Provider store={store}><MemoryRouter><Register /></MemoryRouter></Provider>);
+      expect(screen.getByText('登録する')).toBeInTheDocument();
+    })
+
+    test('名前のラベル確認', () => {
+      render(<Provider store={store}><MemoryRouter><Register /></MemoryRouter></Provider>);
+      expect(screen.getByText('名前')).toBeInTheDocument();
+    })
+
+    test('メールアドレスのラベル確認', () => {
+      render(<Provider store={store}><MemoryRouter><Register /></MemoryRouter></Provider>);
+      expect(screen.getByText('メールアドレス')).toBeInTheDocument();
+    })
+
+    test('パスワードのラベル確認', () => {
+      render(<Provider store={store}><MemoryRouter><Register /></MemoryRouter></Provider>);
       expect(screen.getByText('パスワード')).toBeInTheDocument();
     })
   })
