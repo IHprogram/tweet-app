8426cad2a596497898fc320e13a2672a030cadd2
IHprogram <ihm20202020@gmail.com>
2021-06-23 13:45:24 +0900

    投稿画面で表示されるテキストの確認を行う単体テストの実装

--- src/__test__/tweet.test.js
+++ src/__test__/tweet.test.js
@@ -2,6 +2,7 @@
 // import { useDispatch } from "react-redux";
 import { render, screen } from '@testing-library/react';
 import Header from '../components/header/Header'
+import TweetForm from '../components/TweetForm'
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
@@ -16,7 +17,7 @@ describe('ヘッダーのテキストの確認', () => {
   test('ヘッダーロゴ「Tweet App」が表示されているか確認', () => {
     const loginUser = false;
     render(<Provider store={store}><MemoryRouter><Header loginUser={loginUser} /></MemoryRouter></Provider>);
-    const linkElement = screen.getByText(/Tweet App/i);
+    const linkElement = screen.getByText('Tweet App');
     expect(linkElement).toBeInTheDocument();
   })
 
@@ -24,14 +25,14 @@ describe('ヘッダーのテキストの確認', () => {
     // ログアウト時、stateのloginUserの値はfalseになっている
     const loginUser = false;
     render(<Provider store={store}><MemoryRouter><Header loginUser={loginUser} /></MemoryRouter></Provider>);
-    const linkElement = screen.getByText(/ログイン/i);
+    const linkElement = screen.getByText('ログイン');
     expect(linkElement).toBeInTheDocument();
   })
 
   test('ログアウト時に、新規画面へのリンクボタンが表示されているか確認', () => {
     const loginUser = false;
     render(<Provider store={store}><MemoryRouter><Header loginUser={loginUser} /></MemoryRouter></Provider>);
-    const linkElement = screen.getByText(/新規登録/i);
+    const linkElement = screen.getByText('新規登録');
     expect(linkElement).toBeInTheDocument();
   })
 
@@ -39,15 +40,40 @@ describe('ヘッダーのテキストの確認', () => {
     // ログイン時、stateのloginUserの値はtrueになっている
     const loginUser = true;
     render(<Provider store={store}><MemoryRouter><Header loginUser={loginUser} /></MemoryRouter></Provider>);
-    const linkElement = screen.getByText(/投稿する/i);
+    const linkElement = screen.getByText('投稿する');
     expect(linkElement).toBeInTheDocument();
   })
 
   test('ログイン時に、ログアウトボタンが表示されているか確認', () => {
     const loginUser = true;
     render(<Provider store={store}><MemoryRouter><Header loginUser={loginUser} /></MemoryRouter></Provider>);
-    const linkElement = screen.getByText(/ログアウト/i);
+    const linkElement = screen.getByText('ログアウト');
     expect(linkElement).toBeInTheDocument();
+  })
+})
+
+describe('ツイート投稿画面のテキストの確認', () => {
+  test('タイトルが表示されているかを確認', () => {
+    const loginUserId = 'UserId0123456789';
+    const userName = 'テストユーザー';
+    render(<Provider store={store}><MemoryRouter><TweetForm loginUserId={loginUserId} userName={userName} /></MemoryRouter></Provider>);
+    const linkElement = screen.getByText('投稿');
+    expect(linkElement).toBeInTheDocument();
+  })
+
+  test('投稿ボタンが表示されているかを確認', () => {
+    const loginUserId = 'UserId0123456789';
+    const userName = 'テストユーザー';
+    render(<Provider store={store}><MemoryRouter><TweetForm loginUserId={loginUserId} userName={userName} /></MemoryRouter></Provider>);
+    const linkElement = screen.getByText('投稿する');
+    expect(linkElement).toBeInTheDocument();
+  })
+
+  test('投稿フォームのラベル確認', () => {
+    const loginUserId = 'UserId0123456789';
+    const userName = 'テストユーザー';
+    render(<Provider store={store}><MemoryRouter><TweetForm loginUserId={loginUserId} userName={userName} /></MemoryRouter></Provider>);
+    expect(screen.getByText('投稿内容を入力してください')).toBeInTheDocument();
   })
 })
 
