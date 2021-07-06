da68bf5dc85be459c873011e98f6dbd9442e6a42
IHprogram <ihm20202020@gmail.com>
2021-07-06 10:56:03 +0900

    ツイート更新用フォームを設置

--- src/components/Detail.tsx
+++ src/components/Detail.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   useLocation, useParams
 } from 'react-router-dom';
@@ -7,11 +7,13 @@ import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 import { Button } from "@material-ui/core";
 import Box from '@material-ui/core/Box';
-import { deleteTweet } from '../actions/index';
+import { updateTweet, deleteTweet } from '../actions/index';
 import { useDispatch } from "react-redux";
 import { useHistory } from 'react-router-dom'
 import { Tweet } from '../Types';
-
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import { spacing, sizing } from '@material-ui/system';
 
 interface Props {
   loginUserId: string
@@ -30,8 +32,16 @@ const Detail = ({ loginUserId }: Props) => {
   const userId: string = state.userId;
   const tweetId: string = state.tweetdata.tweetId;
 
+  const [formFlag, setFormFlag] = useState(false);
+  const [newTweet, setNewTweet] = useState('');
+
   const history = useHistory();
   const dispatch = useDispatch();
+
+
+  const theme = {
+    spacing: 10, //「m(margin」や「p(padding)」が「1」につき10pxに設定される。
+  }
 
   const styles = {
     'width': '70vw',
@@ -43,13 +53,29 @@ const Detail = ({ loginUserId }: Props) => {
     'margin': '0 auto'
   }
 
-  const buttonStyle = {
-    'color': 'red',
+  const updateFormStyle = {
+    'width': '100%',
+    'height': '100%',
   }
 
   const deleteButton = () => {
     dispatch(deleteTweet(tweetId))
     history.push('/')
+  }
+
+  const updateFormButton = () => {
+    console.log('更新処理')
+    // history.push('/')
+  }
+
+  const tweetChange = (e) => {
+    const tweet: string = e.target.value;
+    setNewTweet(tweet);
+  }
+
+  const updateButton = () => {
+    console.log('更新処理')
+    // history.push('/')
   }
 
   return (
@@ -65,13 +91,29 @@ const Detail = ({ loginUserId }: Props) => {
           {userId === loginUserId ?
             <Box mx="auto">
               <Button variant="outlined" color="secondary" onClick={deleteButton}>削除</Button>
+              <Button variant="outlined" color="primary" onClick={updateFormButton}>更新</Button>
             </Box>
             :
             (<div></div>
             )}
         </CardContent>
       </Card>
-    </div>
+
+      <Grid container style={styles} alignItems="center" justify="center">
+        <Grid item xs={12}>
+          <form>
+            <Grid container alignItems="center" justify="space-between">
+              <Grid item>
+                <TextField variant="outlined" label='更新内容' onChange={e => tweetChange(e)} style={updateFormStyle} />
+              </Grid>
+              <Grid item>
+                <Button variant="outlined" color="primary" onClick={updateButton} style={updateFormStyle}>更新</Button>
+              </Grid>
+            </Grid>
+          </form>
+        </Grid>
+      </Grid>
+    </div >
   )
 }
 
